eb400e4894de313c66c0def159c5a6ed29986d57
Triston <tristonherbst97@gmail.com>
2021-07-28 16:46:23 -0500

    post request

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const { json } = require('express')
 const express = require('express')
-
 const app = express()
+app.use(express.json()
 
 const courses = [
     {id: 1, name: 'bingo'},
@@ -22,7 +23,14 @@ app.get('/api/courses/:id', (req, res) => {
     res.send(course)
 })
 
-
+app.post('/api/courses',(req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(course)
+    res.send(course)
+})
 
 //Environment variable PORT
 const port = process.env.PORT || 3000
