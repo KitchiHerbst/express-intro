7b599c28db540d916da221fd30d6d4d51aa20aca
Triston <tristonherbst97@gmail.com>
2021-07-28 16:27:30 -0500

    using params with get

--- index.js
+++ index.js
@@ -2,13 +2,26 @@ const express = require('express')
 
 const app = express()
 
+const courses = [
+    {id: 1, name: 'bingo'},
+    {id: 2, name: 'bongo'},
+    {id: 3, name: 'bango'},
+    ]
+
 app.get('/',(req, res) => {
     res.send('Yummy bingo')
 })
 
 app.get('/api/courses', (req, res) => {
-    res.send([1,2,3,4,5,6])
+    res.send(courses)
 })
+
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(obj => obj.id === parseInt(req.params.id))
+    if (!course) res.status(404).send(`Course with the id of ${req.params.id} does not exist`)
+    res.send(course)
+})
+
 
 
 //Environment variable PORT
