8130e4357f2cef1d040a5e698eb9066481c3144c
Triston <tristonherbst97@gmail.com>
2021-07-29 14:16:12 -0500

    handling delete requests

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-const courses = [
+let courses = [
   { id: 1, name: "bingo" },
   { id: 2, name: "bongo" },
   { id: 3, name: "bango" },
@@ -21,21 +21,17 @@ app.get("/api/courses", (req, res) => {
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find((obj) => obj.id === parseInt(req.params.id));
   if (!course)
-    res
+    return res
       .status(404)
       .send(`Course with the id of ${req.params.id} does not exist`);
   res.send(course);
 });
 
 app.post("/api/courses", (req, res) => {
-  const schema = Joi.object({
-    name: Joi.string().min(3).required(),
-  });
+  const { error } = validateCourse(req.body.name);
 
-  const result = schema.validate({ name: req.body.name });
-
-  if (result.error) {
-    res.status(400).send(result.error.details[0].message);
+  if (error) {
+    res.status(400).send(error.details[0].message);
     return;
   }
   const course = {
@@ -46,8 +42,51 @@ app.post("/api/courses", (req, res) => {
   res.send(course);
 });
 
+app.put("/api/courses/:id", (req, res) => {
+  const course = courses.find((obj) => obj.id === parseInt(req.params.id));
+  if (!course) {
+    return res
+      .status(404)
+      .send(`Course with the id of ${req.params.id} does not exist`);
+  }
+
+  const { error } = validateCourse(req.body.name);
+
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+  //update course name
+  course.name = req.body.name;
+  res.send(course);
+});
+
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find((obj) => obj.id === parseInt(req.params.id));
+  if (!course) {
+    return res
+      .status(404)
+      .send(`Course with the id of ${req.params.id} does not exist`);
+      
+  }
+  courses = courses.filter((c) => c.id !== course.id);
+  res.send(courses)
+
+//   const index = courses.indexOf(course)
+//   courses.splice(index, 1)
+//   res.send(courses)
+});
+
 //Environment variable PORT
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Yummy ${port}`);
 });
+
+const validateCourse = (course) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  return schema.validate({ name: course });
+};
