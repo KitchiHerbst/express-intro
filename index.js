309624c4894aacd397196b9039caa1e9a479356d
Triston <tristonherbst97@gmail.com>
2021-07-29 12:20:43 -0500

    joi implementation

--- index.js
+++ index.js
@@ -1,39 +1,59 @@
-const { json } = require('express')
-const express = require('express')
-const app = express()
-app.use(express.json())
+const Joi = require("joi");
+const { json } = require("express");
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+
 
 const courses = [
-    {id: 1, name: 'bingo'},
-    {id: 2, name: 'bongo'},
-    {id: 3, name: 'bango'},
-    ]
+  { id: 1, name: "bingo" },
+  { id: 2, name: "bongo" },
+  { id: 3, name: "bango" },
+];
 
-app.get('/',(req, res) => {
-    res.send('Yummy bingo')
-})
+app.get("/", (req, res) => {
+  res.send("Yummy bingo");
+});
 
-app.get('/api/courses', (req, res) => {
-    res.send(courses)
-})
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
 
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find(obj => obj.id === parseInt(req.params.id))
-    if (!course) res.status(404).send(`Course with the id of ${req.params.id} does not exist`)
-    res.send(course)
-})
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((obj) => obj.id === parseInt(req.params.id));
+  if (!course)
+    res
+      .status(404)
+      .send(`Course with the id of ${req.params.id} does not exist`);
+  res.send(course);
+});
 
-app.post('/api/courses',(req, res) => {
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    }
-    courses.push(course)
-    res.send(course)
-})
+app.post("/api/courses", (req, res) => {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    })
+
+    const result = schema.validate({name: req.body.name})
+    console.log(result)
+
+
+  if (!req.body.name || req.body.name.length < 3) {
+    res
+      .status(400)
+      .send("Name is required and at minimum should be three characters");
+      return
+  }
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
 
 //Environment variable PORT
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Yummy ${port}`)
-})
+  console.log(`Yummy ${port}`);
+});
